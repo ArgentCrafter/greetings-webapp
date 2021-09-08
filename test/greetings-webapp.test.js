c92db68f5aa947a46910c818e70f6372e9cdc55d
Jaden Hope <Jaden_Hope@Yahoo.com>
2021-09-08 10:58:45 +0200

    testing

--- test/greetings-webapp.test.js
+++ test/greetings-webapp.test.js
@@ -1,7 +1,7 @@
 let assert = require('assert');
 const { Pool } = require('pg');
 let factory = require('../greetingsFactory');
-const connectionString = 'postgresql://jaden:codex123@localhost:5432/names_greeted';
+const connectionString = 'postgres://rntohclcqqooug:6e4d677f68a42e8f11d37d49692c964ce319903d08edd57a1e6fbbb394139f1b@ec2-18-215-44-132.compute-1.amazonaws.com:5432/dbfr12j9n97iqk';
 
 const pool = new Pool({
     connectionString
@@ -11,21 +11,17 @@ describe('Tests:', () => {
 
     describe('Database Tests:', () => {
 
-        // beforeEach(async function () {
-            // await pool.query("DROP TABLE testing");
-            // await pool.query("CREATE TABLE testing (id serial primary key not null, value integer not null)")
-        // })
-
-        it('Should pass the insert db test', async function () {
+        beforeEach(async function () {
             await pool.query("DROP TABLE testing");
             await pool.query("CREATE TABLE testing (id serial primary key not null, value integer not null)")
+        })
+
+        it('Should pass the insert db test', async function () {
             await pool.query('INSERT INTO testing (value) values (1)');
             assert.equal(await (await pool.query('SELECT * FROM testing')).rows[0].value, 1);
         })
 
         it('Should pass the update db test', async function () {
-            await pool.query("DROP TABLE testing");
-            await pool.query("CREATE TABLE testing (id serial primary key not null, value integer not null)")
             await pool.query('INSERT INTO testing (value) values (1)');
             await pool.query('UPDATE testing SET value = 2');
             assert.equal(await (await pool.query('SELECT * FROM testing')).rows[0].value, 2);
